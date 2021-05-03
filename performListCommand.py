5f405e43106f58b227c2e33d76e14f5f410678e0
Himanshu Nankani <52562516+HimanshuNankani@users.noreply.github.com>
2021-05-03 12:42:09 +0530

    Create performListCommand.py
    Consider a list (list = []). You can perform the following commands:
    
    1. insert i e: Insert integer e at position 1.
    2. print: Print the list.
    3. remove e: Delete the first occurrence of integer e.
    4. append e: Insert integer e at the end of the list.
    5. sort: Sort the list.
    6. pop: Pop the last element from the list.
    7. reverse: Reverse the list.
    
    Initialise your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. Iterate through each command in order and perform the corresponding operation on your list.
    
    Sample Input 0
    
    12
    insert 0 5
    insert 1 10
    insert 0 6
    print
    remove 6
    append 9
    append 1
    sort
    print
    pop
    reverse
    print
    Sample Output 0
    
    [6, 5, 10]
    [1, 5, 9, 10]
    [9, 5, 1]

---
+++ performListCommand.py
@@ -0,0 +1,19 @@
+for _ in range(N):
+        commandList.append(input())
+    
+    for i in range(N):
+        if commandList[i]=="reverse":
+            ar.reverse()
+        elif commandList[i]=="pop":
+            ar.pop()
+        elif commandList[i]=="sort":
+            ar.sort()
+        elif commandList[i]=="print":
+            print(ar)
+        elif "append" in commandList[i]:
+            ar.append(int(commandList[i][7:]))
+        elif "remove" in commandList[i]:
+            ar.remove(int(commandList[i][7:]))
+        elif "insert" in commandList[i]:
+            x = commandList[i].split()
+            ar.insert(int(x[1]),int(x[2]))
