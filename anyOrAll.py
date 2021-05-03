023fa49e235b303c9f8c68dfcd216c73bbdf863b
Himanshu Nankani <52562516+HimanshuNankani@users.noreply.github.com>
2021-05-03 13:53:48 +0530

    Delete anyOrAll.py

--- anyOrAll.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    nList = list(map(str, input().split()))
-    
-    pos = all([int(x)>0 for x in nList])
-    pal = any([y==y[::-1] for y in nList])
-    print(all([pos,pal]))
